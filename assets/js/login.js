b3b807ebfe1c6a2d7b14e87bffe3c425311357c1
omarq5094 <202308430@stu.jazanu.edu.sa>
2026-03-09 07:18:34 +0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,50 @@
+const SCRIPT_URL = "ضع رابط exec هنا";
 
+const form = document.getElementById("loginForm");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const phone=document.getElementById("phone").value.trim();
+const password=document.getElementById("password").value.trim();
+
+const data=new URLSearchParams();
+
+data.append("phone",phone);
+data.append("password",password);
+data.append("action","login");
+
+try{
+
+const res=await fetch(SCRIPT_URL,{
+method:"POST",
+headers:{
+"Content-Type":"application/x-www-form-urlencoded"
+},
+body:data.toString()
+});
+
+const text=await res.text();
+const result=JSON.parse(text);
+
+if(result.success){
+
+localStorage.setItem("userPhone",phone);
+
+window.location.href="dashboard.html";
+
+}else{
+
+msg.innerText="بيانات الدخول غير صحيحة";
+
+}
+
+}catch(err){
+
+msg.innerText="خطأ في الاتصال";
+
+}
+
+});
