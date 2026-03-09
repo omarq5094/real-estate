6d9de3872d21017130224135d7c52fdd02ff8f3a
omarq5094 <202308430@stu.jazanu.edu.sa>
2026-03-09 07:34:28 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,5 @@
 
+window.APP_CONFIG = {
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwBRXleXMaOtaEsA3m9HfbX2zArAGSvsiaSSp0-1gbgOqE2hiL5_czP5RY3c9VMgOEmCw/exec",
+  WHATSAPP_NUMBER: "966556104669"
+};
